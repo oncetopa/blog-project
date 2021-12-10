752479b0c344649af69e2fd363b5267286f54c04
oncetopa <92215717+oncetopa@users.noreply.github.com>
2021-12-10 23:50:58 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+let itemList = [];
+let inputButton = document.querySelector(".input_button");
+inputButton.addEventListener("click", addItem);
+
+$(".input_section").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $(".input_button").click();
+    }
+});
+
+function addItem() {
+    let item = document.querySelector(".item").value;
+    if (item != null) {
+        itemList.push(item);
+        document.querySelector(".item").value = "";
+        document.querySelector(".item").focus();
+    }
+
+    showList();
+}
+
+function showList() {
+    let list = "<ul>"
+    for (let i = 0; i <itemList.length; i++) {
+        list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">" + "\u00D7" + "</span></li>";
+    }
+    list += "</ul>";
+    document.querySelector(".item_list").innerHTML = list;
+
+
+    let deleteButtons = document.querySelectorAll(".close");
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", deleteItem);
+    }
+
+}
+
+function deleteItem() {
+    let id = this.getAttribute("id");
+    itemList.splice(id, 1);
+    showList();
+}
